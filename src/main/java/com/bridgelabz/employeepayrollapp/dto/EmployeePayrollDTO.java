dd2beed9dc2705d1faa2ddf60e7c5461f72e209a
mehulbhange <bhangemehul@gmail.com>
2022-10-15 23:04:13 +0530

    ADDED:[Mehul] getter and setter auto generated using lombok

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,16 +1,11 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
 public class EmployeePayrollDTO {
     public String name;
     public long salary;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "name = "+name +" salary = " + salary;
-    }
 }
