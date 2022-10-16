0d07ccc596f54e14d779251d414d44bba95971cd
mehulbhange <bhangemehul@gmail.com>
2022-10-16 14:33:51 +0530

    ADDED:[Mehul] determine the logging levels based on app running in dev, Staging or prod

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -3,13 +3,15 @@ package com.bridgelabz.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeepayrollappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeepayrollappApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeepayrollappApplication.class, args);
         log.info("Employee payroll App Started");
+        log.info("Employee payroll App Started in "+ context.getEnvironment().getProperty("environment")+" Environment");
     }
 }
