fbbeb260526a4048aeb033261cc316a4ed054ea6
mehulbhange <bhangemehul@gmail.com>
2022-10-19 20:18:57 +0530

    ADDED:[Mehul] exception handler for incorrect date format

--- src/main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
@@ -2,8 +2,10 @@ package com.bridgelabz.employeepayrollapp.exception;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler {
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -27,6 +30,14 @@ public class EmployeePayrollExceptionHandler {
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
         ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", exception.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid date format", exception);
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",
+                "Should have date in the format dd MMM yyyy (10 Oct 2020)");
         return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 }
