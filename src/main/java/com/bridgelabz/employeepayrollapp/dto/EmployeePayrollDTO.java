c85bc27c65ad66be7734a6207b08512a3531278d
mehulbhange <bhangemehul@gmail.com>
2022-10-19 19:15:29 +0530

    ADDED:[Mehul] validations on the payroll DTO fields

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -16,12 +16,18 @@ public class EmployeePayrollDTO {
     public String name;
     @Min(value = 500, message = "Salary should be more than 500")
     public long salary;
-
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
+    @NotEmpty(message = "Gender should not be Empty")
     public String gender;
     @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "start date should not be Empty")
+    @PastOrPresent(message = "Start date should be past or todays date")
     public LocalDate startDate;
+    @NotBlank(message = "Note cannot be Empty")
     public String note;
+    @NotBlank(message = "profilePic cannot be Empty")
     public String profilePic;
+    @NotNull(message = "department should not be Empty")
     public List<String> departments;
 
 }
